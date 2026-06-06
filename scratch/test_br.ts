cb2d8c6aa73c12a70d7a768aa5d9d4a9b27f8cce
Genkipool <genkipoolcardano@gmail.com>
2026-06-06 20:07:09 +0200

    chore: clean up tracking of scratch scripts and local agent state

--- scratch/test_br.ts
+++
@@ -1,4 +0,0 @@
-import { applyMarkdownToHtml } from '../features/docs/utils/markdownParser';
-
-console.log(applyMarkdownToHtml("Hello<br />World"));
-console.log(applyMarkdownToHtml("• Point 1<br />• Point 2"));
